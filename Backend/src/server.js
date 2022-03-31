6bab30f6a4b20274879fc17f1a6cdfdbeb753ad1
satishkumarojha <satishkumarojha15@gmail.com>
2022-03-31 22:12:37 +0530

    port changed to 5000

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -7,7 +7,7 @@ const connect=require("./configs/db")
 app.listen(5000,async()=>{
     try {
         await connect()
-        console.log("listening to port 4000")
+        console.log("listening to port 5000")
     } catch (err) {
         console.log(err)
         
